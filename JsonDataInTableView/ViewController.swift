fcf5de409301afdce2df04657af7c0417229d164
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-05-16 14:48:40 +0530

    Initial Commit

---
+++ JsonDataInTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonDataInTableView
+//
+//  Created by Zeitech Solutions on 16/05/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
